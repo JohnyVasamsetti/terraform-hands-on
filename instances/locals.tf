0a753b328bdba72ad77ffae0ad2ea0ec32d6bd54
Johny Vasamsetti <johny.vasamsetti@thoughtworks.com>
2024-03-01 19:33:50 +0530

    Add vpc with public and private subnets

---
+++ instances/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  mumbai_vpc_cidr_block = "192.168.0.0/16"
+  public_subnet_cidr_block = "192.168.0.0/24"
+  private_subnet_cidr_block = "192.168.1.0/24"
+}
---
+++ instances/main.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc" "mumbai_vpc" {
+  cidr_block = local.mumbai_vpc_cidr_block
+  tags = {
+    task = "instances"
+    Name = "mumbai_vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.mumbai_vpc.id
+  cidr_block = local.public_subnet_cidr_block
+  availability_zone = "ap-south-1a"
+  tags = {
+    task = "instances"
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.mumbai_vpc.id
+  cidr_block = local.private_subnet_cidr_block
+  tags = {
+    task = "instances"
+    Name = "private_subnet"
+  }
+}
