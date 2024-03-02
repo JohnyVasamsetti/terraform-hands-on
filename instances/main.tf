ead15b8054c95e785b8a92f4ea30cb73a3a03809
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-03-03 00:12:53 +0530

    [Johny,Sravani] Provision private instance along with it's security group

--- instances/main.tf
+++ instances/main.tf
@@ -112,6 +112,18 @@ resource "aws_instance" "public_instance" {
     Name = "public_instance"
   }
 }
+resource "aws_instance" "private_instance" {
+  ami           = local.ami_id
+  instance_type = local.instance_type
+  subnet_id     = aws_subnet.private_subnet.id
+  user_data     = file("user_data.sh")
+  security_groups = [aws_security_group.private_instance_sg.id]
+  key_name = aws_key_pair.instance_key.key_name
+  tags = {
+    task = "instances"
+    Name = "private_instance"
+  }
+}
 
 # Security groups
 resource "aws_security_group" "public_instance_sg" {
@@ -134,6 +146,32 @@ resource "aws_security_group" "public_instance_sg" {
     to_port = -1
     protocol = "icmp"
     cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    task = "instances"
+    Name = "public_instance_sg"
+  }
+}
+resource "aws_security_group" "private_instance_sg" {
+  name = "private_instance_sg"
+  vpc_id = aws_vpc.mumbai_vpc.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [local.public_subnet_cidr_block]
+  }
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = [ local.public_subnet_cidr_block ]
   }
   egress {
     from_port = 0
