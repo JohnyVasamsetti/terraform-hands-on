3b8542029544644473e90e4431edeef76148b67f
sravani <sravanimandela140@gmail.com>
2024-03-04 09:11:39 +0530

    create vpc and subnets along with route tables

---
+++ ALB/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  virginia_vpc_cidr_block     = "192.168.0.0/16"
+  public_subnet_cidr_block    = "192.168.0.0/24"
+  private_subnet_cidr_block   = "192.168.11.0/24"
+  private_subnet_cidr_block_2 = "192.168.12.0/24"
+  region = "us-east-1"
+}
